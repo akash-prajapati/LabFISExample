6dafdb3b0e1efd81b5cf5db2375a60547b143fab
akash-prajapati <akash.prajapati@gmail.com>
2018-07-31 11:41:45 +0530

    Update Application.java

--- src/main/java/io/fabric8/quickstarts/camel/Application.java
+++ src/main/java/io/fabric8/quickstarts/camel/Application.java
@@ -15,27 +15,24 @@
  */
 package io.fabric8.quickstarts.camel;
 
-import org.apache.camel.builder.RouteBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+
+
 
 /**
  * A spring-boot application that includes a Camel route builder to setup the Camel routes
  */
 @SpringBootApplication
 @ImportResource({"classpath:spring/camel-context.xml"})
-public class Application extends RouteBuilder {
 
-    // must have a main method spring-boot can run
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+public class Application {
+	
+   // must have a main method spring-boot can run
+    public static void main(String[] args) {    	
+    	SpringApplication.run(Application.class, args);
+    }  
+   
 
-    @Override
-    public void configure() throws Exception {
-        from("timer://foo?period=5000")
-            .setBody().constant("Hello World")
-            .log(">>> ${body}");
-    }
 }
